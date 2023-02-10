e3dd1b86a8c32212a24fbf08c87f57b163254e16
Your Name <you@example.com>
2023-02-10 15:47:38 +0530

    Checking employee is present or absent

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -6,7 +6,9 @@ namespace EmpWage
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Calculation");
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            Employee.CheckEmployee();
 
             Console.ReadLine();
         }
