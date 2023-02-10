6a17cc54c66a11ce373890c557ecfb2d80539495
Your Name <you@example.com>
2023-02-10 15:56:01 +0530

    Calculating employee daily wage

--- EmpWage/Employee.cs
+++ EmpWage/Employee.cs
@@ -10,20 +10,27 @@ namespace EmpWage
     {
         public static void CheckEmployee()
         {
-
-            // UC -1 checking employee is present or absent
             const int IS_PRESENT = 1;
+            const int EMP_RATE_PER_HR = 20;
+
+            int empHrs = 0, empWage = 0;
+
             Random random = new Random();
             int employeeCheck = random.Next(2);
 
             if (employeeCheck == IS_PRESENT)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee daily wage is : " + empWage);
 
         }
     }
